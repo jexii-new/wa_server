1221e47bd67893942583b722a9e5d86b14fc0874
imronpuji <imronpuji5@gmail.com>
2021-12-06 15:12:18 +0700

    tambahSetHeader

--- routes/index.js
+++ routes/index.js
@@ -101,11 +101,7 @@ router.get('/kontak/delete/:id', async (req, res, next) => {
 
 // groups
 router.get('/group', (req, res, next) => getGroup(async (result) => await res.render('group', {groups:result})))
-router.post('/group', async (req, res, next) => await postGroup(req.body, async (val) =>  
-{
-	return res.redirect('/group')
-}
-))
+router.post('/group', async (req, res, next) => await postGroup(req.body, async (val) =>  res.redirect('/group')))
 router.get('/group/delete/:id', async (req, res, next) => {
 	await getGroupsDetailsById(req.params.id, async (result) => {
 		await result.filter(async val => {
