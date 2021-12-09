da6f966d190eab6e12638b162170f0fb806c3a6e
imronpuji <imronpuji5@gmail.com>
2021-12-09 15:24:50 +0700

    kjh

--- routes/index.js
+++ routes/index.js
@@ -348,7 +348,7 @@ router.post('/setting/edit', (req, res, next) => putProfile(4, req.body, ()=>res
 router.get('/setting/group/:group_id', async (req, res, next) => getGroup(async (result, val) => {
 	getGroupById(req.params.group_id, (resultGroupId) => {
 		getProfile((resProf) => {
-			getSettingGroupById(resultGroupId[0].id, (resGroupsDetail) => res.render('setting_group', {setting_groups:resGroupsDetail, groups:result,url:resProf.domain, group:resultGroupId[0]}))
+			getSettingGroupById(resultGroupId[0].id, (resGroupsDetail) => res.render('setting_group', {setting_groups:resGroupsDetail, groups:result,url:req.protocol + 's://' + req.headers.host, group:resultGroupId[0]}))
 		})
 	})
 }))
