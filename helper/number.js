4e4db3af3fe39ca2ee9534a5843df50c38abe566
imronpuji <imronpuji5@gmail.com>
2021-12-10 11:09:58 +0700

    imron

--- helper/number.js
+++ helper/number.js
@@ -3,19 +3,24 @@
 const numberVerify = (val, char, cb) => {
     const valSplit = val.split('')
     let res = []
-    if(valSplit[0] == '0'){
-      const splice = valSplit.splice(1,99)
-      res.push(...splice)
+    if(valSplit[0] == '6'){
+        return cb(val)
+    } else {
+
+        if(valSplit[0] == '0'){
+          const splice = valSplit.splice(1,99)
+          res.push(...splice)
+        }
+
+        res.unshift('',"6", "2")
+        const result = res.join('')
+        const numbFirst = result.substring(0, 5)
+        const numbSecond = result.substring(5,8)
+        const numbThird = result.substring(8,11)
+        const numbFourth = result.substring(11)
+
+        return cb(numbFirst + char + numbSecond + char + numbThird + char + numbFourth)
     }
-
-    res.unshift('',"6", "2")
-    const result = res.join('')
-    const numbFirst = result.substring(0, 5)
-    const numbSecond = result.substring(5,8)
-    const numbThird = result.substring(8,11)
-    const numbFourth = result.substring(11)
-
-    return cb(numbFirst + char + numbSecond + char + numbThird + char + numbFourth)
     
 }
  
